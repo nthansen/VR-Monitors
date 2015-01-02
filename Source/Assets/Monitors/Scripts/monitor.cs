1ed94de2d98eb3a754f783acd02ff3147d669ae1
Norman Hansen <nthansen@uw.edu>
2015-01-02 01:56:42 -0800

    fixed up the lighting and made it so it updates at every frame

--- Source/Assets/Monitors/Scripts/monitor.cs
+++ Source/Assets/Monitors/Scripts/monitor.cs
@@ -26,6 +26,20 @@ public class monitor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Texture2D tex = new Texture2D(200, 300, TextureFormat.RGB24, false);
+        Rectangle screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+        Bitmap target = new Bitmap(screenSize.Width, screenSize.Height);
+        using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(target))
+        {
+            g.CopyFromScreen(0, 0, 0, 0, new Size(screenSize.Width, screenSize.Height));
+        }
+        System.IO.MemoryStream ms = new System.IO.MemoryStream();
+        target.Save(ms, ImageFormat.Png);
+        ms.Seek(0, SeekOrigin.Begin);
+
+
+        tex.LoadImage(ms.ToArray());
+
+        renderer.material.mainTexture = tex;
 	}
 }
