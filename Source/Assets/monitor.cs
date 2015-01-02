021becdffaafdb6e577ad99edbea92f4b002ce90
Norman Hansen <nthansen@uw.edu>
2015-01-01 17:21:02 -0800

    Not sure if im doing this right but started adding a monitor class to the project

---
+++ Source/Assets/monitor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System;
+
+public class monitor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Texture2D tex = new Texture2D (200, 300, TextureFormat.RGB24, false);
+		UnityEngine.Rect screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+		Bitmap target = new Bitmap (screenSize.width, screenSize.height);
+		using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(target)) {
+			g.CopyFromScreen (0, 0, 0, 0, new Size (screenSize.width, screenSize.height));
+		}
+		System.IO.MemoryStream ms = new System.IO.MemoryStream();
+		target.Save (ms, ImageFormat.Png);
+		ms.Seek (0, SeekOrigin.Begin);
+		
+		
+		tex.LoadImage (ms.ToArray ());
+		
+		renderer.material.mainTexture = tex;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
