44cb5ab04c572327f8bb8df72c352cf80dddda00
Norman Hansen <nthansen@uw.edu>
2015-01-02 06:36:04 -0800

    fixed crashing after running for a bit by making local variables so new is only used in start and not update

--- Source/Assets/Monitors/Scripts/monitor.cs
+++ Source/Assets/Monitors/Scripts/monitor.cs
@@ -6,15 +6,20 @@ using System.IO;
 
 public class monitor : MonoBehaviour {
 
+    Texture2D tex;
+    Rectangle screenSize;
+    Bitmap target;
+    System.IO.MemoryStream ms;
+
 	// Use this for initialization
 	void Start () {
-		Texture2D tex = new Texture2D (200, 300, TextureFormat.RGB24, false);
-		Rectangle screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
-		Bitmap target = new Bitmap (screenSize.Width, screenSize.Height);
+		tex = new Texture2D (200, 300, TextureFormat.RGB24, false);
+		screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+		target = new Bitmap (screenSize.Width, screenSize.Height);
 		using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(target)) {
 			g.CopyFromScreen (0, 0, 0, 0, new Size (screenSize.Width, screenSize.Height));
 		}
-		System.IO.MemoryStream ms = new System.IO.MemoryStream();
+		ms = new System.IO.MemoryStream();
 		target.Save (ms, ImageFormat.Png);
 		ms.Seek (0, SeekOrigin.Begin);
 		
@@ -26,18 +31,13 @@ public class monitor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Texture2D tex = new Texture2D(200, 300, TextureFormat.RGB24, false);
-        Rectangle screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
-        Bitmap target = new Bitmap(screenSize.Width, screenSize.Height);
+        screenSize = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
         using (System.Drawing.Graphics g = System.Drawing.Graphics.FromImage(target))
         {
             g.CopyFromScreen(0, 0, 0, 0, new Size(screenSize.Width, screenSize.Height));
         }
-        System.IO.MemoryStream ms = new System.IO.MemoryStream();
         target.Save(ms, ImageFormat.Png);
         ms.Seek(0, SeekOrigin.Begin);
-
-
         tex.LoadImage(ms.ToArray());
 
         renderer.material.mainTexture = tex;
