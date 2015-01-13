b97927bf5e15e83a56ad9af455ea836fa4a4f0c9
Norman Hansen <nthansen@uw.edu>
2015-01-12 22:43:02 -0800

    added a slider to eventually use for moving the camera

--- Source/Assets/Monitors/Scripts/ExternalForm.cs
+++ Source/Assets/Monitors/Scripts/ExternalForm.cs
@@ -10,11 +10,23 @@ public class ExternalForm : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         current = new System.Windows.Forms.Form();
+        
         Button button1 = new Button();
+
         button1.Text = "This is a test";
+        
+        TrackBar viewSlider = new TrackBar();
+
+        viewSlider.Location = new Point(5, 200);
+
         current.Controls.Add(button1);
+
+        current.Controls.Add(viewSlider);
+
         current.Text = "I am testing";
+        
         current.StartPosition = FormStartPosition.CenterScreen;
+        
         current.Show();
     }
 	
