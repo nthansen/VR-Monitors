85027c51a72ab2540e4b6e40cfbef90f06893c65
Norman Hansen <nthansen@uw.edu>
2015-01-12 22:21:32 -0800

    Added the exteral windows form, this will be used to modify the camera view

---
+++ Source/Assets/Monitors/Scripts/ExternalForm.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Drawing;
+using System.Windows.Forms;
+
+public class ExternalForm : MonoBehaviour {
+
+    Form current;
+
+	// Use this for initialization
+	void Start () {
+        current = new System.Windows.Forms.Form();
+        Button button1 = new Button();
+        button1.Text = "This is a test";
+        current.Controls.Add(button1);
+        current.Text = "I am testing";
+        current.StartPosition = FormStartPosition.CenterScreen;
+        current.Show();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
