856b57b4f790c50ec698afcf42006e96da48719f
Norman Hansen <nthansen@uw.edu>
2015-01-15 10:38:33 -0800

    added a function that allows unity to detect if the crosshair is pointing at anything, eventually to allow us to select and move monitors

--- Source/Assets/Scripts/Reticle.cs
+++ Source/Assets/Scripts/Reticle.cs
@@ -18,5 +18,18 @@ public class Reticle : MonoBehaviour {
         // rotate so we can always see the plain since it's 2d
         transform.Rotate(0.0f, 180.0f, 0.0f);
 
+        reticleHit();
+    }
+
+    // Used to see if the reticle is pointing at a monitor
+    void reticleHit()
+    {
+        Ray ray = CameraFacing.ViewportPointToRay(new Vector3(0.5f,0.5f,0f));
+		// Do a raycast
+		RaycastHit hit;
+		if (Physics.Raycast(ray, out hit))
+			print ("I'm looking at " + hit.transform.name);
+		else
+			print ("I'm looking at nothing!");
     }
 }
