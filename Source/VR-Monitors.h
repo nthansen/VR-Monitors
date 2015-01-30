f4f7ece3f1a59198cfb198682f8a80cb0a1bcdf1
nthansen <nthansen@uw.edu>
2015-01-30 13:05:20 -0800

    starting to rewrite everything our own way

---
+++ Source/VR-Monitors.h
@@ -0,0 +1,56 @@
+#ifndef VR-Monitors
+#define VR-Monitors
+
+#include "OVR_Kernel.h"
+
+#include "../Assets/CommonSrc/Platform/Platform_Default.h"
+#include "../Assets/CommonSrc/Render/Render_Device.h"
+#include "../Assets/CommonSrc/Render/Render_XmlSceneLoader.h"
+#include "../Assets/CommonSrc/Platform/Gamepad.h"
+#include "../Assets/CommonSrc/Util/OptionMenu.h"
+#include "../Assets/CommonSrc/Util/RenderProfiler.h"
+
+#include "Util/Util_Render_Stereo.h"
+using namespace OVR::Util::Render;
+
+#include "Sensors/OVR_DeviceConstants.h"
+
+// Filename to be loaded by default, searching specified paths.
+#define WORLDDEMO_ASSET_FILE  "VR-Monitors.xml"
+
+#define WORLDDEMO_ASSET_PATH1 "Assets/VR-Monitors/"
+#define WORLDDEMO_ASSET_PATH2 "../../Assets/VR-Monitors/"
+
+using namespace OVR;
+using namespace OVR::OvrPlatform;
+using namespace OVR::Render;
+
+class VRMonitorsApp : public Application
+{
+public:
+
+	VRMonitorsApp();
+	~VRMonitorsApp();
+
+private: 
+
+	virtual int  OnStartup(int argc, const char** argv);
+	virtual void OnIdle();
+
+	virtual void OnMouseMove(int x, int y, int modifiers);
+	virtual void OnKey(OVR::KeyCode key, int chr, bool down, int modifiers);
+	virtual void OnResize(int width, int height);
+
+	bool         SetupWindowAndRendering(int argc, const char** argv);
+
+	enum SceneRenderMode
+	{
+		Scene_World
+	};
+	SceneRenderMode    SceneMode;
+
+
+};
+
+
+#endif
