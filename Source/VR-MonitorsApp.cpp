a1f22c808abed76e408d0a75d5aafb9d1874c233
nthansen <nthansen@uw.edu>
2015-01-30 13:24:57 -0800

    starting to comment everything for the startup so whats going on is clear

--- Source/VR-MonitorsApp.cpp
+++ Source/VR-MonitorsApp.cpp
@@ -1,14 +1,15 @@
 #include "VR-MonitorsApp.h"
 
+// when the app starts it'll call this function to initialize everything for the oculus
+
 int VRMonitorsApp::OnStartup(int argc, const char** argv) {
+	
+	// name the thread so whenever we access it, we can use this thread name
 	OVR::Thread::SetCurrentThreadName("OWDMain");
 
-	// *** Oculus HMD & Sensor Initialization
+	// Get everything organized for displaying onto the oculus
 
-	// Create DeviceManager and first available HMDDevice from it.
-	// Sensor object is created from the HMD, to ensure that it is on the
-	// correct device.
-
+// if the user is using windows 32 bit we need to set the thread to the highest priority
 #if defined(OVR_OS_WIN32)
 	OVR::Thread::SetCurrentPriority(Thread::HighestPriority);
 
@@ -18,41 +19,44 @@ int VRMonitorsApp::OnStartup(int argc, const char** argv) {
 	}
 #endif
 
+	// initialize the ovr library
 	ovr_Initialize();
 
+	// create a connection to the oculus from the library
 	Hmd = ovrHmd_Create(0);
 
+	// if it doesn't detect and oculus then we need to simulate one
 	if (!Hmd)
 	{
-
-		// If we didn't detect an Hmd, create a simulated one for debugging.
 		Hmd = ovrHmd_CreateDebug(ovrHmd_DK2);
+
+		// check it the debug one was created
 		if (!Hmd)
-		{   // Failed Hmd creation.
+		{   // if not then exit with 1 so we know something went wrong
 			return 1;
 		}
 	}
 
+	// we know the oculus is using extended desktop so the size of our window is the size of the oculus resolution
 	if (Hmd->HmdCaps & ovrHmdCap_ExtendDesktop)
 	{
 		WindowSize = Hmd->Resolution;
 	}
+	// otherwise it's what we create it as
 	else
 	{
-		// In Direct App-rendered mode, we can use smaller window size,
-		// as it can have its own contents and isn't tied to the buffer.
-		WindowSize = Sizei(1100, 618);//Sizei(960, 540); avoid rotated output bug.
+		WindowSize = Sizei(1100, 618);
 	}
 
 
-	// ***** Setup System Window & rendering.
+	// create the window to start displaying everything
 
 	if (!SetupWindowAndRendering(argc, argv))
 	{
+		// if it didn't work then return 1 to say there was an error
 		return 1;
 	}
 
-
-
+	// everything went smooth so exit fine
 	return 0;
 }
