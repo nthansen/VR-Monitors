174900814f5a833e5661e2276ea8ae50ca074448
nthansen <nthansen@uw.edu>
2015-02-04 15:36:00 -0800

    setup to read the texturecube but still need to figure out how to seperate the textures to each side of the cube

--- DirectX Version/Source/scene.cpp
+++ DirectX Version/Source/scene.cpp
@@ -22,6 +22,16 @@ Scene::Scene() : num_models(0) // Main world
 		"Texture2D Texture   : register(t0); SamplerState Linear : register(s0); "
 		"float4 main(in float4 Position : SV_Position, in float4 Color: COLOR0, in float2 TexCoord : TEXCOORD0) : SV_Target"
 		"{   return Color * Texture.Sample(Linear, TexCoord); }";
+	char* VertexShaderSkybox =
+		"float4x4 Proj, View;"
+		"float4 NewCol;"
+		"void main(in  float4 Position  : POSITION,    in  float4 Color : COLOR0, in  float3 TexCoord  : TEXCOORD0,"
+		"          out float4 oPosition : SV_Position, out float4 oColor: COLOR0, out float3 oTexCoord : TEXCOORD0)"
+		"{   oPosition = mul(Proj, mul(View, Position)); oTexCoord = TexCoord; oColor = Color; }";
+	char* PixelShaderSkybox =
+		"TextureCube textureCube   : register(t0); SamplerState Linear : register(s0); "
+		"float4 main(in float4 Position : SV_Position, in float4 Color: COLOR0, in float3 TexCoord : TEXCOORD0) : SV_Target"
+		"{   return Color * textureCube.Sample(Linear, TexCoord); }";
 
 	// Construct textures
 	static Model::Color tex_pixels[4][256 * 256];
@@ -50,28 +60,15 @@ Scene::Scene() : num_models(0) // Main world
 	ID3D11ShaderResourceView* shaderResource;
 
 	//CreateDDSTextureFromFile(DX11.Device, L"Assets/skybox.dds", &resource, &shaderResource);//if skyboxCubeMapDDS is used the image is warped in all directions, but only shows one "face" of the cubemap on all sides
-	CreateDDSTextureFromFileEx(DX11.Device, L"Assets/skyboxCubeMapDDS.dds", 0U, D3D11_USAGE_DEFAULT, D3D11_BIND_SHADER_RESOURCE, 0, D3D11_RESOURCE_MISC_TEXTURECUBE, true, &resource, NULL);
+	CreateDDSTextureFromFileEx(DX11.Device, L"Assets/skyboxCubeMapDDS.dds", 0U, D3D11_USAGE_DEFAULT, 
+		D3D11_BIND_SHADER_RESOURCE, 0, D3D11_RESOURCE_MISC_TEXTURECUBE, true, &resource, &shaderResource);
 	
 	ID3D11Texture2D* tex2d;
 	
 	resource->QueryInterface(IID_ID3D11Texture2D, (void **)&tex2d);
 
-	// use only when making a cube map
-
-	D3D11_TEXTURE2D_DESC SMTextureDesc;
-	tex2d->GetDesc(&SMTextureDesc);
-
-	D3D11_SHADER_RESOURCE_VIEW_DESC SMViewDesc;
-	SMViewDesc.Format = SMTextureDesc.Format;
-	SMViewDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURECUBE;
-	SMViewDesc.TextureCube.MipLevels = SMTextureDesc.MipLevels;
-	SMViewDesc.TextureCube.MostDetailedMip = 0;
-
-	DX11.Device->CreateShaderResourceView(tex2d, &SMViewDesc, &shaderResource);
-
-
 	ImageBuffer* t = new ImageBuffer(true, true, Sizei(256, 256), tex2d, shaderResource);
-	generated_texture[4] = new ShaderFill(ModelVertexDesc, 3, VertexShaderSrc, PixelShaderSrc, t);
+	generated_texture[4] = new ShaderFill(ModelVertexDesc, 3, VertexShaderSkybox, PixelShaderSkybox, t);
 
 
 	// Construct geometry
