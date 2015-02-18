a424dd5c514383966b49da76c8a7b09f7639c605
kingozzy24 <kingozzy24@gmail.com>
2015-02-18 13:21:24 -0800

    flush

--- DirectX Version/Source/desktop.cpp
+++ DirectX Version/Source/desktop.cpp
@@ -48,7 +48,7 @@ int Desktop::getFrame(FRAME_DATA* data, bool* timedout) {
     DXGI_OUTDUPL_FRAME_INFO frameData;
     HRESULT hr;
 
-    hr = desktop->AcquireNextFrame(10, &frameData, &desktopResource);
+    hr = desktop->AcquireNextFrame(0, &frameData, &desktopResource);
     if (hr == DXGI_ERROR_WAIT_TIMEOUT)
     {
         *timedout = true;
