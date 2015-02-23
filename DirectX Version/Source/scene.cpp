90ebde3ad8a786b56e7ccbdf8d749f58c8d91df6
Quig <djraccoon@gmail.com>
2015-02-22 18:41:58 -0800

    allright we can pick up monitors by looking at them

--- DirectX Version/Source/scene.cpp
+++ DirectX Version/Source/scene.cpp
@@ -19,7 +19,7 @@ int Scene::pickMonitor(Vector3f Pos, float Yaw){
 	//Model *target = nullptr;//model we will find
 	//save the direction to cast in dir and position to start from in dpos
 	SimpleMath::Vector3 dPos = SimpleMath::Vector3(Pos.x, Pos.y, Pos.z);//position for simplemath
-	SimpleMath::Vector3 dir = SimpleMath::Vector3(sinf(Yaw), 0, cosf(Yaw - PI));
+	SimpleMath::Vector3 dir = SimpleMath::Vector3(sinf(Yaw-PI), 0, cosf(Yaw-PI));
 	//ray is really only a container holding the origin and direction to cast
 	SimpleMath::Ray cast = SimpleMath::Ray(dPos, dir);
 	//crate a bounding box around each monitor to see if we hit one
@@ -30,13 +30,13 @@ int Scene::pickMonitor(Vector3f Pos, float Yaw){
 	float _dist = 0.0f;
 	for (i; i < num_monitors; i++){
 		Model *temp = Monitors[i];
-		DirectX::BoundingOrientedBox mbox = BoundingOrientedBox(XMFLOAT3(temp->Pos.x, temp->Pos.y, temp->Pos.z), XMFLOAT3(.4f, .4f, 0.0f), XMFLOAT4(sinf(Yaw), 0, cosf(Yaw - PI), 0));
+		DirectX::BoundingOrientedBox mbox = BoundingOrientedBox(XMFLOAT3(temp->Pos.x, temp->Pos.y, temp->Pos.z), XMFLOAT3(.5f, .5f, 0), XMFLOAT4(0, temp->Rot.y, 0, temp->Rot.w));
 		DirectX::BoundingSphere mSphere = BoundingSphere(XMFLOAT3(temp->Pos.x, temp->Pos.y, temp->Pos.z), monitorHeight);
 		//Model * m = new Model(Vector3f(0, 0, startingPoint.z1), generated_texture[1]); // for bounding box
 		//m->AddSolidColorBox(temp->Pos.x, temp->Pos.y, temp->Pos.z, temp->Pos.x + monitorWidth ,
 		//	temp->Pos.y + monitorHeight, temp->Pos.z + monitorDepth, Model::Color(0,128,128,1));//to visualize the selected monitor
 		//m->AllocateBuffers(); Add(m); selected = m;
-		if (mSphere.Intersects(cast.position, cast.direction, _dist)){
+		if (mbox.Intersects(cast.position, cast.direction, _dist)){
 			//					*target = *temp; //causes error but may want to save reference
 			//loop causes freeze need to get a reference and keep moving the monitor someplace
 			//while (controlPanel.movingMonitor){
